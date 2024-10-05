bbc0ca06eeb60a38fb7226a033f61feb1235d81b
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-10-05 23:28:17 +0800

    feat

--- main/apps/web-app/src/app/layout.tsx
+++ main/apps/web-app/src/app/layout.tsx
@@ -2,27 +2,29 @@ import PageContainer from "@/components/PageContainer"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Navbar from "@/components/navigation/navbar";
+import '@rainbow-me/rainbowkit/styles.css';
+import { Providers } from './providers';
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-    title: "NebulaID: Authentic, Anonymous Hotel Review",
-    description: "An authentic, anonymous hotel review platform on Ethereum.",
-    icons: { icon: "/tomaru.svg", apple: "/tomaru.png" },
+    title: "NebulaID",
+    description: " A InterPlantery Decentralized Identity Protocol  ",
+    icons: { icon: "/nebula.png", apple: "/nebula.png" },
     metadataBase: new URL("https://demo.semaphore.pse.dev"),
-    openGraph: {
-        type: "website",
-        url: "https://demo.semaphore.pse.dev",
-        title: "NebulaID",
-        description: "An authentic, anonymous hotel review platform on Ethereum.",
-        siteName: "NebulaID",
-        images: [
-            {
-                url: "https://demo.semaphore.pse.dev/social-media.png"
-            }
-        ]
-    },
-    twitter: { card: "summary_large_image", images: "https://demo.semaphore.pse.dev/social-media.png" }
+    // openGraph: {
+    //     type: "website",
+    //     url: "https://demo.semaphore.pse.dev",
+    //     title: "NebulaID",
+    //     description: "A InterPlantery Decentralized Identity Protocol",
+    //     siteName: "NebulaID",
+    //     images: [
+    //         {
+    //             url: "https://demo.semaphore.pse.dev/social-media.png"
+    //         }
+    //     ]
+    // },
 }
 
 export default function RootLayout({
@@ -33,7 +35,15 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body suppressHydrationWarning className={inter.className}>
-                <PageContainer>{children}</PageContainer>
+                <Providers>
+                    <PageContainer>
+                        <div className="min-h-screen bg-gradient-to-r from-blue-300 via-green-200 to-yellow-300 bg-opacity-50 ">
+                            <Navbar />
+                            {children}
+                        </div>
+
+                    </PageContainer>
+                </Providers>
             </body>
         </html>
     )
--- main/apps/web-app/src/app/page.tsx
+++ main/apps/web-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Head from 'next/head'
 import Link from 'next/link'
-import { BsArrowRight } from 'react-icons/bs'
+// import { BsArrowRight } from 'react-icons/bs'
 import { useRouter } from "next/navigation"
 import { useCallback, useContext, useEffect, useState } from "react"
 import LogsContext from "../context/LogsContext"
@@ -40,7 +40,62 @@ export default function HomePage() {
     }, [_reviewers])
 
     return (
-        <>
+        <div className='p-[10rem]'>
+            <div className="hero-content text-black text-center">
+                <div className="flex flex-col w-max">
+                    <div className="mb-5 text-5xl font-serif font-bold"> Get Your Universal Profile 
+                    </div>
+                    <div className="mb-5 text-xl">
+                        Verify your onchain & offchain data for identity, credit score and healtcare data using TLSNotary & Worldcoin.
+                    </div>
+                    <div className='pt-10'>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div className="bg-white p-6 rounded-xl shadow-xl ">
+                                <Link
+                                    href="/loan?state=lend">
+                                    <div className="bg-white p-6 rounded-xl ">
+                                        <h2 className="text-2xl font-semibold mb-4">For Stakers</h2>
+                                        <p>Just liquid stake your EDU tokens and get onchain yields and platform rewards.</p>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="bg-white p-6 rounded-xl shadow-xl ">
+                                <Link
+                                    href="/loan?state=borrow">
+                                    <div className="bg-white p-6 rounded-xl ">
+                                        <h2 className="text-2xl font-semibold mb-4">For Yielders</h2>
+                                        <p>Simple, Easy, Effective - Yield vaults of USDC which gives you upto <b>60% APY</b></p>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='py-5'>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div className="bg-white p-6 rounded-xl shadow-xl ">
+                                <Link
+                                    href="/loan?state=lend">
+                                    <div className="bg-white p-6 rounded-xl ">
+                                        <h2 className="text-2xl font-semibold mb-4">For Lenders</h2>
+                                        <p>Earn competitive interest rates by providing liquidity to the community.</p>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="bg-white p-6 rounded-xl shadow-xl ">
+                                <Link
+                                    href="/loan?state=borrow">
+                                    <div className="bg-white p-6 rounded-xl ">
+                                        <h2 className="text-2xl font-semibold mb-4">For Borrowers</h2>
+                                        <p>Access loans backed by cryptocurrency assets at favorable rates.</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
             <h2 className="font-size: 3rem;">Authentic, Anonymous Hotel Reviews</h2>
 
             <div className="summary">
@@ -59,10 +114,10 @@ export default function HomePage() {
 
             <div className="text-top">
                 <h3><a
-                        href="https://www.agoda.com/v-hotel-bencoolen/hotel/singapore-sg.html"
-                        target="_blank"
-                        rel="noreferrer noopener nofollow"
-                    >V Hotel Bencoolen, Singapore</a>
+                    href="https://www.agoda.com/v-hotel-bencoolen/hotel/singapore-sg.html"
+                    target="_blank"
+                    rel="noreferrer noopener nofollow"
+                >V Hotel Bencoolen, Singapore</a>
                 </h3>
                 {_reviews.length > 0 && (
                     <button className="button-link" onClick={createReview}>
@@ -101,6 +156,6 @@ export default function HomePage() {
 
             <h3>Other hotels...</h3>
 
-        </>
+        </div>
     )
 }
